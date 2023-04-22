c180a24e510452eef4202d5b4278492c71ead382
Damanpreet Singh Grewal <damanpreetsinghgrewal92@gmail.com>
2023-04-22 16:19:16 -0400

    Adding state

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,29 +1,38 @@
 'use client';
 
+import { useState, useCallback } from 'react';
+
 import Avatar from '../Avatar';
+
 import { AiOutlineMenu } from 'react-icons/ai';
 
 const UserMenu = () => {
-    return (
-        <div className='relative'>
-            <div className='flex flex-row items-center gap-3'>
-                <div
-                    onClick={() => { }}
-                    className='hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer'
-                >
-                    Airbnb your home
-                </div>
-                <div
-                    onClick={() => { }}
-                    className='p-4 md:py-1 md:px-2 border-[1px] border-neutral-300 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition'
-                >
-                    <AiOutlineMenu />
-                    <div className='hidden md:block'>
-                        <Avatar />
-                    </div>
-                </div>
-            </div>
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen(value => !value);
+  }, []);
+
+  return (
+    <div className='relative'>
+      <div className='flex flex-row items-center gap-3'>
+        <div
+          onClick={() => {}}
+          className='hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer'
+        >
+          Airbnb your home
         </div>
-    );
+        <div
+          onClick={() => {}}
+          className='p-4 md:py-1 md:px-2 border-[1px] border-neutral-300 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition'
+        >
+          <AiOutlineMenu />
+          <div className='hidden md:block'>
+            <Avatar />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default UserMenu;
