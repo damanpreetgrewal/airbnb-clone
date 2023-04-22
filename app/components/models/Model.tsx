43e693f074ee458130fe9872541c09d81f1369fd
Damanpreet Singh Grewal <damanpreetsinghgrewal92@gmail.com>
2023-04-22 17:14:46 -0400

    Model Component

---
+++ app/components/models/Model.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useState } from 'react';
+
+interface ModelProps {
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  actionLabel?: string;
+  disabled?: boolean;
+  secondaryAction?: () => void;
+  secondaryLabel?: string;
+}
+
+const Model: React.FC<ModelProps> = ({
+  isOpen,
+  onClose,
+  onSubmit,
+  title,
+  body,
+  footer,
+  actionLabel,
+  disabled,
+  secondaryAction,
+  secondaryLabel,
+}) => {
+  const [showModel, setShowModel] = useState(false);
+  
+  return <div>Model</div>;
+};
+export default Model;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import { Nunito } from 'next/font/google';
 
 import './globals.css';
 import Navbar from './components/navbar/Navbar';
+import ClientOnly from './components/ClientOnly';
+import Model from './components/models/Model';
 
 export const metadata = {
   title: 'Airbnb',
@@ -20,7 +22,10 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <Model />
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
