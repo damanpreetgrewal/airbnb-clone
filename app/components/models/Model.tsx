c4861f3a9b6fc3857139779e745e281b03c8e2c5
Damanpreet Singh Grewal <damanpreetsinghgrewal92@gmail.com>
2023-04-25 01:37:06 -0400

    Model Component

--- app/components/models/Model.tsx
+++ app/components/models/Model.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 interface ModelProps {
   isOpen?: boolean;
@@ -27,8 +27,57 @@ const Model: React.FC<ModelProps> = ({
   secondaryAction,
   secondaryLabel,
 }) => {
-  const [showModel, setShowModel] = useState(false);
+  const [showModel, setShowModel] = useState(isOpen);
 
-  return <div>Model</div>;
+  useEffect(() => {
+    setShowModel(isOpen);
+  }, [isOpen]);
+
+  const handleClose = useCallback(() => {
+    if (disabled) {
+      setShowModel(false);
+      setTimeout(() => {
+        onClose();
+      }, 300);
+    }
+  }, [disabled, onClose]);
+
+  const handleSubmit = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+    onSubmit();
+  }, [disabled, onSubmit]);
+
+  const handleSecondaryAction = useCallback(() => {
+    if (disabled || !secondaryAction) {
+      return;
+    }
+    secondaryAction();
+  }, [disabled, secondaryAction]);
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <>
+      <div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70'>
+        <div className='relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto'>
+          {/* {Content} */}
+          <div
+            className={`translate duration-300 h-full ${
+              showModel ? 'translate-y-0' : 'translate-y-full'
+            }
+              ${showModel ? 'opacity-100' : 'opacity-0'}
+              `}
+          >
+            <div className='translate h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
+
 export default Model;
--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang='en'>
       <body className={font.className}>
         <ClientOnly>
-          <Model />
+          <Model isOpen />
           <Navbar />
         </ClientOnly>
         {children}
