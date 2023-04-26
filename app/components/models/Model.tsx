d1a813ff489ba3cd1114647e16eff891d9242d74
Damanpreet Singh Grewal <damanpreetsinghgrewal92@gmail.com>
2023-04-26 02:41:11 -0400

    Model Component

--- app/components/models/Model.tsx
+++ app/components/models/Model.tsx
@@ -63,7 +63,7 @@ const Model: React.FC<ModelProps> = ({
   return (
     <>
       <div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70'>
-        <div className='relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto'>
+        <div className='relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-7 mx-auto h-full lg:h-auto md:h-auto'>
           {/* {Content} */}
           <div
             className={`translate duration-300 h-full ${
