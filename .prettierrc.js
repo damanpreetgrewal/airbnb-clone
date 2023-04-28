d2d4cb28158934633bc39a5f51b83ff0a98ae2ad
Damanpreet Singh Grewal <damanpreetsinghgrewal92@gmail.com>
2023-04-28 00:48:32 -0400

    Button component

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    arrowParens: "always",
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: "none",
+    printWidth: 120,
+    trailingComma: "es5",
+  };
--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { IconType } from 'react-icons';
 
 interface ButtonProps {
@@ -9,23 +11,23 @@ interface ButtonProps {
   icon?: IconType;
 }
 
-const Button: React.FC<ButtonProps> = ({
-  label,
-  onClick,
-  disabled,
-  outline,
-  small,
-  icon,
-}) => {
+const Button: React.FC<ButtonProps> = ({ label, onClick, disabled, outline, small, icon: Icon }) => {
   return (
     <button
+      onClick={onClick}
+      disabled={disabled}
       className={`relative disabled:opacity-70 disabled:cursor-not-allowed rounded-lg hover:opacity-80 transition w-full 
       ${outline ? 'bg-white' : 'bg-rose-500'}
       ${outline ? 'border-black' : 'border-rose-500'}
       ${outline ? 'text-black' : 'text-white'}
+      ${small ? 'py-1' : 'py-3'}
+      ${small ? 'text-sm' : 'text-md'}
+      ${small ? 'font-light' : 'font-semibold'}
+      ${small ? 'border-[1px]' : 'border-2'}
        
        `}
     >
+      {Icon && <Icon size={24} className="left-4 absolute top-3" />}
       {label}
     </button>
   );
