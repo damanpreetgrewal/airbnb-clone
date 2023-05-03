8845ba737e5a523a52bb23f7f8e445ec2f950e18
Damanpreet Singh Grewal <damanpreetsinghgrewal92@gmail.com>
2023-05-02 23:31:08 -0400

    Build fixes try 3

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -5,6 +5,6 @@ interface ContainerProps {
 }
 
 const Container: React.FC<ContainerProps> = ({ children }) => {
-  return <div className="max-w-[2520px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4 ">{children}</div>;
+  return <div className="max-w-[2510px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4 ">{children}</div>;
 };
 export default Container;
