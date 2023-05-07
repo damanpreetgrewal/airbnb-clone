993168387abdad06101436446e3217dfaf244632
Damanpreet Singh Grewal <damanpreetsinghgrewal92@gmail.com>
2023-05-07 01:25:48 -0400

    Register Modal

---
+++ app/components/inputs/Input.tsx
@@ -0,0 +1,6 @@
+'use client';
+
+const Input = () => {
+  return <div></div>;
+};
+export default Input;
--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -8,6 +8,8 @@ import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
+import Heading from '../Heading';
+import Input from '../inputs/Input';
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -41,6 +43,12 @@ const RegisterModal = () => {
       });
   };
 
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading title="Welcome to Airbnb" subtitle="Create an Account!" />
+      <Input />
+    </div>
+  );
   return (
     <Modal
       disabled={isLoading}
@@ -49,6 +57,7 @@ const RegisterModal = () => {
       actionLabel="Continue"
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
     />
   );
 };
