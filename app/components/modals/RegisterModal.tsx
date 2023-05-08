1bcbfcf22e05d5c8a2dfaa60d3549652a7cb4523
Damanpreet Singh Grewal <damanpreetsinghgrewal92@gmail.com>
2023-05-07 20:02:48 -0400

    Input Component and its state

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -46,7 +46,9 @@ const RegisterModal = () => {
   const bodyContent = (
     <div className="flex flex-col gap-4">
       <Heading title="Welcome to Airbnb" subtitle="Create an Account!" />
-      <Input />
+      <Input id="email" label="Email" disabled={isLoading} register={register} errors={errors} required />
+      <Input id="name" label="Name" disabled={isLoading} register={register} errors={errors} required />
+      <Input id="password" type="password" label="Password" disabled={isLoading} register={register} errors={errors} required />
     </div>
   );
   return (
