49b82c835aedf6fc3ca2509351ac318ced9d0204
Damanpreet Singh Grewal <damanpreetsinghgrewal92@gmail.com>
2023-05-07 20:27:19 -0400

    Prisma DB init

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -70,7 +70,9 @@ const RegisterModal = () => {
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="justify-center flex flex-row items-center gap-2">
           <div>Already have an account?</div>
-          <div className="text-neutral-800 cursor-pointer hover:underline">Log In</div>
+          <div onClick={registerModal.onClose} className="text-neutral-800 cursor-pointer hover:underline">
+            Log In
+          </div>
         </div>
       </div>
     </div>
