0aaa0bfeaba73c551ce0309df07ac0e6c63293be
Damanpreet Singh Grewal <damanpreetsinghgrewal92@gmail.com>
2023-05-10 01:04:19 -0400

    Signin google and github

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -2,8 +2,12 @@
 
 import Image from 'next/image';
 
-const Avatar = () => {
-  return <Image className="rounded-full" height="30" width="30" alt="Avatar" src="/images/placeholder.jpg" />;
+interface AvatarProps {
+  src: string | null | undefined;
+}
+
+const Avatar: React.FC<AvatarProps> = ({ src }) => {
+  return <Image className="rounded-full" height="30" width="30" alt="Avatar" src={src || '/images/placeholder.jpg'} />;
 };
 
 export default Avatar;
--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import axios from 'axios';
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { AiFillGithub } from 'react-icons/ai';
@@ -71,8 +70,22 @@ const LoginModal = () => {
   const footerContent = (
     <div className="flex flex-col gap-4 mt-3">
       <hr />
-      <Button outline label="Continue with Google" icon={FcGoogle} onClick={() => {}} />
-      <Button outline label="Continue with Github" icon={AiFillGithub} onClick={() => {}} />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {
+          signIn('google');
+        }}
+      />
+      <Button
+        outline
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => {
+          signIn('github');
+        }}
+      />
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="justify-center flex flex-row items-center gap-2">
           <div>Already have an account?</div>
