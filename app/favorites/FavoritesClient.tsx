67100bb1b4da53d87fad6a6e7cdec5a11acc1799
Damanpreet Singh Grewal <damanpreetsinghgrewal92@gmail.com>
2023-07-16 02:18:19 -0400

    Favorites page

--- app/favorites/FavoritesClient.tsx
+++ app/favorites/FavoritesClient.tsx
@@ -25,6 +25,11 @@ const FavoritesClient: React.FC<FavoritesClientProps> = ({
             mt-10
             grid
             grid-cols-1
+            sm: grid-cols-2
+            md: grid-cols-3
+            lg: grid-cols-4
+            xl: grid-cols-5
+            2xl: grid-cols-6
             gap-8
             '
       >
@@ -32,7 +37,7 @@ const FavoritesClient: React.FC<FavoritesClientProps> = ({
           <ListingCard
             currentUser={currentUser}
             key={listing.id}
-            data={listing}
+            data={listing   }
           />
         ))}
       </div>
